77585e6b651d09bb8197b153f4212734b3c1f8d1
Christopher Mann <cmann2@gmail.com>
2019-10-03 14:38:01 -0700

    fix star-wars text

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {ApiService} from './../api.service';
 import { Router } from '@angular/router';
 import { fadeInAnimation } from '../animations';
+import { element } from 'protractor';
 
 @Component({
   selector: 'app-auth',
@@ -26,6 +27,10 @@ export class AuthComponent implements OnInit {
   overridePulsing =  false;
   showVideo = false;
   silentCountdown = true;
+  timer = 0;
+  creedIndex = 0;
+  creed = [];
+  creedClone = null;
 
   constructor(private service : ApiService, private router: Router) { }
 
@@ -43,7 +48,38 @@ export class AuthComponent implements OnInit {
           this.authNote = msgs[1];
           this.footerImage = '../../assets/img/seal.png';
           if (res["creed"]) {
-            this.scrollingText = res["creed"];
+            this.creed = res["creed"].split('\t');
+            this.scrollingText = this.creed[0];
+            var el = document.getElementById('star-wars');
+            if (el) {
+              this.creedClone = el.cloneNode(true);
+            }
+            setInterval(() => {
+              this.timer++;
+              if (this.timer > 36) {
+                this.timer = 0;
+                var temp = this.scrollingText;
+                if (this.creedIndex >= this.creed.length) {
+                  this.creedIndex = 0;
+                }
+                this.scrollingText = this.creed[this.creedIndex++];
+                el = document.getElementById('star-wars');
+                console.log('here we are');
+                setTimeout(() => {
+                  el.style.animation = 'none';
+                  el.offsetHeight; /* trigger reflow */
+                  el.style.animation = null; 
+                  // //Hack see: https://css-tricks.com/restart-css-animation/
+                  // el.classList.remove("crawl");
+                  // void el.offsetWidth;
+                  // el.classList.add("crawl");
+                  // el.before(el.cloneNode(true));
+                  // el.className += " active";
+                  //setTimeout(() => el.className.replace(" active", ""), 118);
+                  
+                }, 118);
+              }
+            }, 1000);
           }
           this.authImage = '../../assets/img/learning.png';
           this.initialized = true;
@@ -63,6 +99,7 @@ export class AuthComponent implements OnInit {
           }, 1000 * this.overridePulseRate);
         }
         setTimeout(() => this.toggleVideo(), 1242);
+        
       }
     });
   }  
